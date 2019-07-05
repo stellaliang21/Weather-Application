32e57d20989cef917b8696d3ee1faf38719bcc45
Stella Liang <stellaliang@Stellas-MacBook-Pro.local>
2019-07-05 15:12:51 -0700

    Change the format of the code

--- client/src/components/DisplayWeather.jsx
+++ client/src/components/DisplayWeather.jsx
@@ -3,16 +3,12 @@ import moment from 'moment';
 import '../styles/DisplayWeather.css';
 
 const DisplayWeather = ( { weather, location } ) => {
-  const svgImage = `https://www.metaweather.com/static/img/weather/${weather.weather_state_abbr}.svg`;
-  const date = weather.applicable_date;
-  const weatherState = weather.weather_state_name;
-  const temperature = ((weather.max_temp * 9/5) + 32).toFixed(1) + '°F';
-  const displayWeather = weather.map((weather) => 
+  const displayWeather = weather.map(weather => 
     <div className='weather' key={weather.id}>
-      <img src={svgImage}/>
-      <div>{moment(date, 'YYYY-MM-DD').format('MMMM Do')}</div>
-      <div>{weatherState}</div>
-      <div>{temperature}</div>
+      <img src={`https://www.metaweather.com/static/img/weather/${weather.weather_state_abbr}.svg`}/>
+      <div>{moment(weather.applicable_date, 'YYYY-MM-DD').format('MMMM Do')}</div>
+      <div>{weather.weather_state_name}</div>
+      <div>{((weather.max_temp * 9/5) + 32).toFixed(1) + '°F'}</div>
     </div>
   )
   return (
@@ -25,4 +21,4 @@ const DisplayWeather = ( { weather, location } ) => {
   )
 } 
 
-export default DisplayWeather
+export default DisplayWeather;
