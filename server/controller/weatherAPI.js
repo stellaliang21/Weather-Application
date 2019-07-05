8d21059930b82a30a4babe2b6060e67291523de8
Stella Liang <stellaliang@Stellas-MacBook-Pro.local>
2019-07-05 15:17:07 -0700

    Change error message

--- server/controller/weatherAPI.js
+++ server/controller/weatherAPI.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
 
 const weatherAPI = (req, res) => {
-  const location = req.params.location
+  const location = req.params.location;
   axios.get(`https://www.metaweather.com/api/location/search/?query=${location}`)
   .then(response1 => { 
     axios.get(`https://www.metaweather.com/api/location/${response1.data[0].woeid}`)
     .then(response2 => res.send(response2.data))
-    .catch(err => console.log(err));
+    .catch(err => res.status(404).end());
   })
-  .catch(err => console.log(err));
+  .catch(err => res.status(404).end());
 };
 
 module.exports = { weatherAPI };
